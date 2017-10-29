8253ca83272b1ffa496f3a91bce6e0593166221b
venkateshwarlu <venkateshwarlu.d@tavant.com>
2017-10-29 17:39:10 -0400

    angular feature modules implementation and optimization

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { SharedModule } from '../shared/shared.module';
+import { AppRoutingModule } from '../app-routing.module';
+import { RecipeService } from '../recipes/recipe.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@NgModule({
+    declarations:[
+        HomeComponent,
+        HeaderComponent
+    ],
+    imports: [
+       SharedModule,
+       AppRoutingModule
+    ],
+    exports:[
+        AppRoutingModule,
+        HeaderComponent
+    ],
+    providers: [ShoppingListService, RecipeService, DataStorageService, AuthService]
+})
+export class CoreModule{
+
+}
